66fb6595930e4693e1cecdc21ec8c48cf5bb24e2
Brandon Thayer <brandon.thayer@pnnl.gov>
2018-04-26 08:23:56 -0700

    - Updated gridappsd-python submodule - Updating README to cover submodules, Python, and Python packages. - First very rough draft at a module for interfacing with the platform, gossCIM.py.

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
+# Dependencies and Installation
+## Submodule: "gridappsd-python"
+In order to interact with the GridAPPS-D platform, the pyvvo application depends on the ["gridappsd-python"](https://github.com/GRIDAPPSD/gridappsd-python) repository and its package, "gridappsd." This repository has been included in this one as a [git submodule](https://git-scm.com/docs/git-submodule). After cloning the gridappsd-pyvvo repository, you'll need to change directories into the top level "gridappsd-python" directory and enter two git commands:
+```Shell Session
+$ git submodule init
+$ git submodule update
+```
+
+Alternatively, while cloning the gridappsd-pyvvo repository, you can pass the `--recurse-submodules` option to the `git clone` command like so:
+```Shell Session
+$ git clone --recurse-submodules https://github.com/GRIDAPPSD/gridappsd-pyvvo.git
+```
+
+The simplest way to keep the "gridappsd-python" repository up to date within this repository is by running:
+```Shell Session
+$ git submodule update --remote gridappsd-python
+```
+
+## Python
+This application works in Python 3. It is recommended that you use the latest version, which was 3.6.5 at the time of writing. On Ubuntu, installation of Python 3.6.x requires the addition of [Felix Krull's deadsnakes PPA](https://launchpad.net/~deadsnakes/+archive/ubuntu/ppa):
+```Shell Session
+$ sudo add-apt-repository ppa:deadsnakes/ppa
+$ sudo apt-get update
+$ sudo apt-get install python3.6
+```
+
+## Python packages
+There is an included "requirements.txt" file in the "pyvvo" directory. Note that the "gridappsd-python" also has a "requirements.txt" file, so we'll need to install those packages too. Python's pip package manager can be used to easily install packages. Here's an example if you're installing the packages for the entire system:
+```Shell Session
+$ cd ~/gridappsd-pyvvo/pyvvo
+$ sudo -H python3.6 -m pip install -r requirements.txt
+$ cd ../gridappsd-python
+$ sudo -H python3.6 -m pip install -r requirements.txt
+```
+
 # MySQL Configuration
 Ensure that the MySQL global variables `innod_db_buffer_pool_size` and `innodb_buffer_pool_instances` are set adequately high.
 On Brandon's Windows machine, `innod_db_buffer_pool_size=4G` and `innodb_buffer_pool_instances=16`
---
+++ pyvvo/app/gossCIM.py
@@ -0,0 +1,103 @@
+'''
+Created on Apr 25, 2018
+
+@author: thay838
+'''
+import sys
+import os
+import simplejson as json
+import time
+#*******************************************************************************
+# Import gridappsd-python
+# Get this directory
+F = os.path.abspath(__file__)
+D = os.path.dirname(F)
+
+# We need to import from gridappsd-python, which should be two levels
+# above the 'app' directory.
+
+# Walk up until we're in 'app' (we should be to start)
+while os.path.basename(D) != 'app':
+    D = os.path.dirname(D)
+    if D == '/':
+        # TODO: this won't work on Windows.
+        raise UserWarning('Walked up to root, no directory named "app"')
+
+# Strip two entries from the 'D' to effectively move up to the top of the
+# repository.
+for _ in range(2):
+    D = os.path.dirname(D)
+    
+# Add gridappsd-python to the path.
+sys.path.insert(0, os.path.join(D, 'gridappsd-python'))
+
+from gridappsd import GridAPPSD
+#*******************************************************************************
+# CONSTANTS
+# Topic for simulation log?
+SIM_LOG = "/topic/goss.gridappsd.process.log.simulation"
+SIM_OUT = "/topic/goss.gridappsd.fncs.output"
+#*******************************************************************************
+# Class for listening to the GOSS bus and taking action 
+class GOSSListener(object):
+    def __init__(self, simID, measDict, appName):
+        """Initialize a GOSSListener."""
+        # Set properties from inputs
+        self.measDict = measDict
+        self.simID = simID
+        self.appName = appName
+        
+        # Create a GridAPPSD object and subscribe to simulation output.
+        self.gossObj = GridAPPSD(simulation_id=self.simID, source=self.appName,
+                                 base_simulation_status_topic=SIM_LOG)
+        self.gossObj.subscribe(SIM_OUT, self)
+        
+        # Initialize dictionary for storing simulation data
+        # TODO: we probably shouldn't stack this up in memory... recipe for 
+        # disaster.
+        self.data = {}
+    
+    def on_message(self, headers, msg):
+        """Forward relevant information along."""
+        # Get the message as a dictionary.
+        msg = json.loads(msg)
+        
+        # Grabe the timestamp
+        t = int(msg['timestamp'])
+        
+        # Ensure we don't have this timestamp already.
+        try:
+            self.data[t]
+        except KeyError:
+            # Good, we don't have the key. Create it.
+            self.data[t] = {}
+        else:
+            # This key already exists? We're in trouble...
+            raise UserWarning("We already have data for time {}".format)
+        
+        # Get the simulation output as a dictionary.
+        # May need to wrap this in a try-catch, TBD
+        simOut = json.loads(msg['output'])
+        
+        # Loop over our measurements, extract info from the sim output
+        for k in self.measDict:
+            self.data[t][k] = simOut[self.simID][k]
+            print(simOut[self.simID][k])
+            
+if __name__ == '__main__':
+    obj = GOSSListener(simID='715107754',
+                       measDict={"cap_capbank2b": 'stuff'},
+                       appName='pyvvo')
+    msg = {"command": "nextTimeStep", "currentTime": 0}
+    for x in range(8):
+        time.sleep(1)
+        msg["currentTime"] += 1
+        print('Incremented time, sending to FNCS.')
+        obj.gossObj.send("goss.gridappsd.fncs.input", json.dumps(msg))
+        
+    # Let's inspect the the object's data dictionary.
+    print('Do it!')
+    
+    # Just wait...
+    time.sleep(60)
+    
