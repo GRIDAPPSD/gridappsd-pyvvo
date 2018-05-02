6a66fdc9329d201fa214957828f0e57ce949581a
Brandon Thayer <brandon.thayer@pnnl.gov>
2018-05-02 09:03:05 -0700

    Further cleaned up zipModel.py, built-in some tests into test_zipMode.py

--- pyvvo/app/zipModel.py
+++ pyvvo/app/zipModel.py
@@ -5,6 +5,9 @@ Created on Apr 27, 2018
 
 Original R code written by Dave Engel. R code adapted to Python by Bin Zheng.
 Final adoption into application by Brandon Thayer.
+
+Notes from Dave:
+Augmented Lagrangian Adaptive Barrier Minimization
 '''
 import numpy as np
 import math
@@ -14,81 +17,106 @@ from scipy.optimize import minimize
 # Constant for ZIP coefficients. ORDER MATTERS!
 ZIPTerms = ['impedance', 'current', 'power']
 
-def zip_mystic(V, P, Q, Vn=240.0):
-    # constants
-    par0 = np.ones(6)*(1/18)  # initial parameters for ZIP model
-    # scale voltage by nominal volt. (Vn) and the power by the base power (Sn) 
-    Sn = np.median(np.sqrt(np.multiply(P,P) + np.multiply(Q,Q)))
-    Vbar = V/Vn
-    Pbar = P/Sn
-    Qbar = Q/Sn
+# List of available solvers for zipFit
+SOLVERS = ['fmin_powell', 'SLSQP']
 
-    sol = my.fmin_powell(Object, args=(Vbar, Pbar, Qbar), x0=par0, constraint=Constrain, disp=False,
-                         gtol=3, ftol=1e-8, full_output=True)
-    # Another algorithm:
-    #sol = my.fmin(Object, args=(Vbar, Pbar, Qbar), x0=par0, constraint=Constrain, disp=False,
-    #              gtol=3, ftol=1e-8, full_output=True)
-          
-    xopt = sol[0]
-    fopt = sol[1]
-    p = xopt[0:3]
-    q = xopt[3:6]
+# Constants for convergence tolerance
+FTOL = 1e-8
+GTOL = 5 # Number of iterations without change for fmin_powell
 
-    coeff = polyToGLD(p, q)
-    coeff['base_power'] = Sn
-    coeff['error'] = fopt
-    coeff['poly'] = (p, q)
+def zipFit(V, P, Q, Vn=240.0, solver='fmin_powell'):
+    """Solve for ZIP coefficients usable by GridLAB-D.
     
-    return coeff
-
-def zip_scipy(V, P, Q, Vn=240.0):
+    V: voltage magnitude array
+    P: real power array
+    Q: reactive power array
+    Vn: nominal voltage
+    solver: either 'fmin_powell' to use mystic's modified scipy fmin_powell 
+        solver, or 'SLSQP' to use scipy's sequential least squares programming
+        solver.
+    """
+    # Estimate nominal power
+    Sn = estimateNominalPower(P=P, Q=Q)
     
-    par0 = np.ones(6)*(1/18)  # initial parameters for ZIP model
-    # scale voltage by nominal volt. (Vn) and the power by the base power (Sn) 
-    Sn = np.median(np.sqrt(np.multiply(P,P) + np.multiply(Q,Q)))
+    # Massage into standard polynomial format
     Vbar = V/Vn
     Pbar = P/Sn
     Qbar = Q/Sn
     
-    # fit ZIP model
-    # Minimize a scalar function of one or more variables using 
-    # Sequential Least SQuares Programming (SLSQP).
-    cons = {'type':'eq', 'fun': Constrain}
-    cons2 = {'type':'eq', 'fun': Constrain2}
-    cons3 = {'type':'eq', 'fun': Constrain3}
+    # Initial parameters for ZIP model
+    # TODO: initialize from previously computed coefficients to start.
+    # Why are we multiply by 1/18?
+    par0 = np.ones(6)*(1/18)
     
-    #print(args[0])
-    opti = minimize(Object, par0, args=(Vbar, Pbar, Qbar), method='SLSQP', constraints = cons, bounds = None)
-    
-    if abs(Constrain(opti.x))>0.75:
-        opti = minimize(Object, par0, args=(V1bar, P1bar, Q1bar), method='SLSQP', constraints = cons2, bounds = None)
-                        
-    if abs(Constrain(opti.x))>2.0:
-        opti = minimize(Object, par0, args=(V1bar, P1bar, Q1bar), method='SLSQP', constraints = cons3, bounds = None)
+    # Solve.
+    if solver == 'fmin_powell':
+        sol = my.fmin_powell(Object, args=(Vbar, Pbar, Qbar), x0=par0,
+                             contraint=Constrain, disp=False, gtol=GTOL,
+                             ftol=FTOL, full_output=True)
+        # Extract the polynomial coefficients
+        p = sol[0][0:3]
+        q = sol[0][3:6]
         
-    # convert back to original scale
-    # opti$par[1] = Z%*cos(Zo), opti$par[2] = I%*cos(Io), opti$par[3] = P%*cos(Po)
-    # opti$par[4] = Z%*sin(Zo), opti$par[5] = I%*sin(Io), opti$par[6] = P%*sin(Po)
-    p = opti.x[0:3]
-    q = opti.x[3:6]
-    sci_opt = opti.fun
-    
+        # Get the value of the objective function (so the squared error)
+        err = sol[1]
+        
+    elif solver == 'SLSQP':
+        sol = minimize(Object, par0, args=(Vbar, Pbar, Qbar), method='SLSQP',
+                       constraints={'type':'eq', 'fun': Constrain},
+                       bounds=None, options={'ftol': FTOL})
+        
+        # Extract the polynomial coefficients
+        p = sol.x[0:3]
+        q = sol.x[3:6]
+        
+        # Get the value of the objective function (so the squared error)
+        err = sol.fun
+    else:
+        raise UserWarning('Given solver, {}, is not implemented.'.format(solver))
+        
+        # Some code Bin had in?
+        #cons2 = {'type':'eq', 'fun': Constrain2}
+        #cons3 = {'type':'eq', 'fun': Constrain3}
+        """
+        if abs(Constrain(opti.x))>0.75:
+            opti = minimize(Object, par0, args=(V1bar, P1bar, Q1bar), method='SLSQP', constraints = cons2, bounds = None)
+                        
+        if abs(Constrain(opti.x))>2.0:
+            opti = minimize(Object, par0, args=(V1bar, P1bar, Q1bar), method='SLSQP', constraints = cons3, bounds = None)
+        """
+        
+    # Convert the polynomial coefficients to GridLAB-D format (fractions and
+    # power factors)
     coeff = polyToGLD(p, q)
+    
+    # Collect other useful information
     coeff['base_power'] = Sn
-    coeff['error'] = sci_opt
-    coeff['poly'] = (p, q)
+    coeff['error'] = err
+    coeff['poly'] = (*p, *q)
     
     return coeff
+
+def estimateNominalPower(P, Q):
+    """Given a set of apparent power measurements, estimate nominal power.
+    
+    For now, we'll simply use the median of the apparent power.
+    """
+    Sn = np.median(np.sqrt(np.multiply(P,P) + np.multiply(Q,Q)))
+    return Sn
 
 def Object(Params, Vbar, Pbar, Qbar):
+    """Objective function for minimization. Minimize squared error."""
     a1, a2, a3, b1, b2, b3 = Params
     return sum( (Pbar - (a1*(Vbar*Vbar)+a2*Vbar+a3))**2
                + (Qbar - (b1*(Vbar*Vbar)+b2*Vbar+b3))**2 )/len(Vbar)
     
 def Constrain(Params):
+    """Constraint for ZIP modeling - """
     a1, a2, a3, b1, b2, b3 = Params
     return math.sqrt(a1*a1 + b1*b1) + math.sqrt(a2*a2 + b2*b2) + math.sqrt(a3*a3 + b3*b3) - 1.0
 
+# These functions aren't currently being used.
+'''
 def Constrain2(Params):
     a1, a2, a3, b1, b2, b3 = Params
     return math.sqrt(a1*a1 + b1*b1) + math.sqrt(a2*a2 + b2*b2) + math.sqrt(a3*a3 + b3*b3) - 2.0
@@ -96,6 +124,7 @@ def Constrain2(Params):
 def Constrain3(Params):
     a1, a2, a3, b1, b2, b3 = Params
     return math.sqrt(a1*a1 + b1*b1) + math.sqrt(a2*a2 + b2*b2) + math.sqrt(a3*a3 + b3*b3) - 3.0
+'''
 
 
 def polyToGLD(p, q):
@@ -107,7 +136,14 @@ def polyToGLD(p, q):
     to flip the imaginary component of apparent power.
     
     NOTE: we're counting on coefficients to come in in 'a, b, c' order, AKA
-    impedance, current, power
+    impedance, current, power.
+    
+    So:
+        p = (a1, a2, a3)
+        q = (b1, b2, b3)
+        a1 = Z%cos(thetaZ), b1 = Z%sin(thetaZ)
+        a2 = I%cos(thetaI), b2 = I%sin(thetaI)
+        a3 = P%cos(thetaP), b4 = P%sin(thetaP)
     """
     # Initialize return
     out = {}
@@ -115,21 +151,29 @@ def polyToGLD(p, q):
     # Track index. Note we're 
     i = 0
     for k in ZIPTerms:
+        # Initialize the fraction. Note that this reduces correctly, but loses
+        # sign information:
+        # a1 = Z%*cos(thetaZ), b1 = Z%*sin(thetaZ) and so on.
         fraction = math.sqrt(p[i]*p[i]+q[i]*q[i])
-        pf = 1.0
-        if fraction!=0:
+        
+        # Derive the power-factor:
+        try:
             pf = abs(p[i]/fraction)
-            # match what is done in Gridlab-D
-        if p[i]>0 and q[i]<0:
+        except ZeroDivisionError:
+            # If we divided by zero, simply make the power factor 1
+            pf = 1
+            
+        # match what is done in Gridlab-D
+        if p[i] > 0 and q[i] < 0:
             # Leading power factor
-            pf = (-1.0)*pf
-        elif p[i]<0 and q[i]<0:
+            pf *= -1
+        elif p[i] < 0 and q[i] < 0:
             # Negative load, flip the fraction
-            fraction = (-1.0)*fraction
-        elif p[i]<0 and q[i]>0:
+            fraction *= -1
+        elif p[i] < 0 and q[i] > 0:
             # Negative load and leading power factor, flip both.
-            pf = (-1.0)*pf
-            fraction = (-1.0)*fraction
+            pf *= -1
+            fraction *= -1
         
         # Assign to return.
         out[k + '_fraction'] = fraction
@@ -154,7 +198,7 @@ def gldZIP(V, coeff, Vn):
         real = coeff['base_power']*coeff[k+'_fraction']*abs(coeff[k+'_pf'])
         imag = real * math.sqrt(1/coeff[k+'_pf']**2 - 1)
         
-        if coeff[k+'_pf'] < 0:
+        if coeff[k +'_pf'] < 0:
             imag *= -1
             
         d[k] = (real, imag)
@@ -181,13 +225,12 @@ if __name__ == '__main__':
     #**************************************************************************
     # Constant current
     I = 1+1j
-    Z = V / I
     S = V * np.conjugate(I)
     P_constI = np.real(S)
     Q_constI = np.imag(S)
-    m = zip_mystic(V, P_constI, Q_constI)
+    m = zipFit(V, P_constI, Q_constI, solver='fmin_powell')
     P_M, Q_M = gldZIP(V, m, Vn)
-    s = zip_scipy(V, P_constI, Q_constI)
+    s = zipFit(V, P_constI, Q_constI, solver='SLSQP')
     P_S, Q_S = gldZIP(V, s, Vn)
     print('Finished constant current test.')
     #**************************************************************************
@@ -197,9 +240,9 @@ if __name__ == '__main__':
     S = V * np.conjugate(I)
     P_constI = np.real(S)
     Q_constI = np.imag(S)
-    m = zip_mystic(V, P_constI, Q_constI)
+    m = zipFit(V, P_constI, Q_constI, solver='fmin_powell')
     P_M, Q_M = gldZIP(V, m, Vn)
-    s = zip_scipy(V, P_constI, Q_constI)
+    s = zipFit(V, P_constI, Q_constI, solver='SLSQP')
     P_S, Q_S = gldZIP(V, s, Vn)
     print('Finished constant impedance test.')
     #**************************************************************************
@@ -207,11 +250,30 @@ if __name__ == '__main__':
     S = np.ones_like(V) * 1+1j
     P_constI = np.real(S)
     Q_constI = np.imag(S)
-    m = zip_mystic(V, P_constI, Q_constI)
+    m = zipFit(V, P_constI, Q_constI, solver='fmin_powell')
     P_M, Q_M = gldZIP(V, m, Vn)
-    s = zip_scipy(V, P_constI, Q_constI)
+    s = zipFit(V, P_constI, Q_constI, solver='SLSQP')
     P_S, Q_S = gldZIP(V, s, Vn)
     print('Finished constant power test.')
     #**************************************************************************
     # Mixture
+    # Constant impedance:
+    Z = 1+1j
+    I_z = V /Z
+    S_z = V * np.conjugate(I_z)
+    # Constant current:
+    I = 1+1j
+    S_i = V * np.conjugate(I)
+    # Constant power.
+    S_p = np.ones_like(V) * 1+1j
+    # Combine
+    S_tot = S_z + S_i + S_p
+    P_tot = np.real(S_tot)
+    Q_tot = np.imag(S_tot)
+    m = zipFit(V, P_tot, Q_tot, solver='fmin_powell')
+    P_M, Q_M = gldZIP(V, m, Vn)
+    s = zipFit(V, P_tot, Q_tot, solver='SLSQP')
+    P_S, Q_S = gldZIP(V, s, Vn)
+    print('Finished constant impedance test.')
+    
     # TODO
---
+++ pyvvo/tests/test_zipModel.py
@@ -0,0 +1,152 @@
+'''
+Created on May 1, 2018
+
+@author: thay838
+'''
+# Get the parent directory on the path:
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+
+import unittest
+import numpy as np
+
+import zipModel
+
+# Get simple voltage array
+V = np.arange(0.9*240, 1.1*240)
+# Nominal voltage is 240V
+Vn = 240
+
+# Tolerance for fit. Let's go with 1%
+RTOL=0.01
+
+# Initialize array for testing.
+testData = []
+
+# Create constant impedance, constant current, constant power, and mixed test
+# cases.
+#******************************************************************************
+# CONSTANT IMPEDANCE TEST 1
+Z = 1+1j
+I = V / Z
+S = V * np.conjugate(I)
+testData.append((np.real(S), np.imag(S), 'Constant Impedance Test 1'))
+# CONSTANT IMPEDANCE TEST 2
+Z = 0.2-15j
+I = V / Z
+S = V * np.conjugate(I)
+testData.append((np.real(S), np.imag(S), 'Constant Impedance Test 2'))
+# CONSTANT IMPEDANCE TEST 3 (exporting power)
+Z = -1200+70j
+I = V / Z
+S = V * np.conjugate(I)
+testData.append((np.real(S), np.imag(S), 'Constant Impedance Test 3'))
+#******************************************************************************
+# CONSTANT CURRENT TEST 1
+I = 1+1j
+S = V*np.conjugate(I)
+testData.append((np.real(S), np.imag(S), 'Constant Current Test 1'))
+# CONSTANT CURRENT TEST 2
+I = 75-4j
+S = V*np.conjugate(I)
+testData.append((np.real(S), np.imag(S), 'Constant Current Test 2'))
+# CONSTANT CURRENT TEST 3 (exporting power)
+I = -213.542+12.15j
+S = V*np.conjugate(I)
+testData.append((np.real(S), np.imag(S), 'Constant Current Test 3'))
+#******************************************************************************
+# CONSTANT POWER TEST 1
+S = 1+1j
+testData.append((np.real(S), np.imag(S), 'Constant Power Test 1'))
+# CONSTANT POWER TEST 2
+S = 15000.12-230.25j
+testData.append((np.real(S), np.imag(S), 'Constant Power Test 2'))
+# CONSTANT POWER TEST 3 (exporting power)
+S = -234+42j
+#******************************************************************************
+# MIXED TEST 1
+# Constant impedance:
+Z = 1+1j
+I_z = V /Z
+S_z = V * np.conjugate(I_z)
+# Constant current:
+I = 1+1j
+S_i = V * np.conjugate(I)
+# Constant power.
+S_p = np.ones_like(V) * 1+1j
+# Combine
+S = S_z + S_i + S_p
+testData.append((np.real(S), np.imag(S), 'Mixed Test 1'))
+# MIXED TEST 2
+# Constant impedance:
+Z = 120-12j
+I_z = V /Z
+S_z = V * np.conjugate(I_z)
+# Constant current:
+I = 72+10j
+S_i = V * np.conjugate(I)
+# Constant power.
+S_p = np.ones_like(V) * 1300-1700j
+# Combine
+S = S_z + S_i + S_p
+testData.append((np.real(S), np.imag(S), 'Mixed Test 2'))
+# MIXED TEST 3
+# Constant impedance:
+Z = 0.06-30j
+I_z = V /Z
+S_z = V * np.conjugate(I_z)
+# Constant current:
+I = -120-15j
+S_i = V * np.conjugate(I)
+# Constant power.
+S_p = np.ones_like(V) * 15+3j
+# Combine
+S = S_z + S_i + S_p
+testData.append((np.real(S), np.imag(S), 'Mixed Test 3'))
+#******************************************************************************
+
+class Test(unittest.TestCase):
+
+
+    def test_zipFit(self):
+        """Test the zipFit function by giving it inputs from polynomials."""
+        # Loop over test cases
+        for t in testData:
+            # Loop over solvers
+            for s in zipModel.SOLVERS:
+                with self.subTest('Test Case: {}, Solver: {}'.format(t[2], s)):
+                    # Get coefficients
+                    coeff = zipModel.zipFit(V=V, P=t[0], Q=t[1], Vn=Vn,
+                                            solver=s)
+                    # Get GridLAB-D's understanding of P and Q based on V and
+                    # the provided coefficients
+                    P, Q = zipModel.gldZIP(V, coeff, Vn=Vn)
+                    
+                    # Test closeness
+                    resultP = np.allclose(t[0], P, rtol=RTOL)
+                    resultQ = np.allclose(t[1], Q, rtol=RTOL)
+                    result = resultP and resultQ
+                    
+                    # Craft a message.
+                    if not (resultP or resultQ):
+                        # Both failed.
+                        msg = 'Neither P nor Q match for {} with the {} solver.'
+                    elif not resultP:
+                        # P failed.
+                        msg = 'P does not match for {} with the {} solver.'
+                    elif not resultQ:
+                        # Q failed.
+                        msg = 'Q does not match for {} with the {} solver.'
+                    else:
+                        # Success.
+                        msg = 'Success for {} with the {} solver.'
+                    
+                    # Execute the test assertion.
+                    self.assertTrue(result, msg.format(t[2], s))
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
